9e0fa26d36cd84e86d73151e0e9b14c6cf62363a
Victor-starr <v94625230@gmail.com>
2024-11-14 17:54:24 +0200

    3cs live chat

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/emoil/', 
+// });
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   base: '/emoil/', 
+  server: {
+    proxy: {
+      // Proxy requests to the 3CX server
+      '/api': {
+        target: 'https://emoilplovdiv.3cx.ro',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''), // removes '/api' prefix
+      },
+    },
+  },
 });
